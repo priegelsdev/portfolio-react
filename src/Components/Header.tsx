59c07ba4b402cf07bd288d3cc9caa76b73d047f3
priegelsdev <priegelsdev@gmail.com>
2023-03-12 15:32:42 +0100

    fill header component with content and some styling

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -2,11 +2,15 @@ import { Link, NavLink } from 'react-router-dom';
 
 export default function Header() {
   return (
-    <header>
-      <nav>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="about">About</NavLink>
-        <NavLink to="projects">Projects</NavLink>
+    <header className="bg-secondary text-primary">
+      <nav className="flex justify-around items-center font-bold py-2 md:py-9 mx-2 md:mx-20 lg:mx-60">
+        <NavLink to="/">home</NavLink>
+        <NavLink to="about">about</NavLink>
+        <Link to="/" className="text-3xl">
+          PhR
+        </Link>
+        <NavLink to="projects">projects</NavLink>
+        <button className="font-medium">🌞</button>
       </nav>
     </header>
   );
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FCF7F8',
+        secondary: '#2c2c2c',
+      },
+    },
   },
   plugins: [],
 };
