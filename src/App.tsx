43e6907dca4d7d0aaeb2f2dbe0762518e5d3aff8
priegelsdev <priegelsdev@gmail.com>
2023-03-12 15:07:44 +0100

    add react router and set up some routes and a layout component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,27 @@
+import React from 'react';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route,
+} from 'react-router-dom';
+
+import Layout from './Components/Layout';
+
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
+
 export default function App() {
-  return <div className="bg-red-500">hello</div>;
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+      </Route>
+    )
+  );
+
+  return <RouterProvider router={router} />;
 }
--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,3 +1,13 @@
+import { Link, NavLink } from 'react-router-dom';
+
 export default function Header() {
-  return <div></div>;
+  return (
+    <header>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="about">About</NavLink>
+        <NavLink to="projects">Projects</NavLink>
+      </nav>
+    </header>
+  );
 }
