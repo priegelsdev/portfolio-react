172c905acff78efc2b8c667507671540ca9b2f18
priegelsdev <priegelsdev@gmail.com>
2023-03-14 11:44:58 +0100

    add images to projects

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,13 +1,18 @@
 import { Link } from 'react-router-dom';
 
+import Trivia from '../img/trivia.png';
+import SkyRides from '../img/skyrides.png';
+import Wordle from '../img/wordle.png';
+import Portfolio from '../img/portfolio.png';
+
 export default function Projects() {
   return (
     <div className="flex bg-primary p-6">
       <div className="grid grid-cols-1 grid-rows-4 md:grid-cols-2 md:grid-rows-2 m-auto">
         <div className="flex flex-col items-center py-2 px-4">
           <img
-            className="mb-2"
-            src="https://placehold.co/300x200"
+            className="mb-2 drop-shadow-lg"
+            src={Trivia}
             alt="picture of project"
           />
           <h2 className="text-xl font-bold mb-2">Trivia Trove</h2>
@@ -17,18 +22,26 @@ export default function Projects() {
             different components.
           </p>
           <div className="flex gap-2">
-            <a className="cursor-pointer bg-secondary text-primary font-semibold rounded-md py-0.5 px-4">
+            <a
+              className="cursor-pointer bg-secondary text-primary font-semibold rounded-md py-0.5 px-4"
+              href="https://trivia-trove.netlify.app"
+              target="_blank"
+            >
               <span className="font-medium">💿</span> Demo
             </a>
-            <a className="cursor-pointer bg-secondary text-primary font-semibold rounded-md py-0.5 px-4">
+            <a
+              className="cursor-pointer bg-secondary text-primary font-semibold rounded-md py-0.5 px-4"
+              href="https://github.com/priegelsdev/quiz"
+              target="_blank"
+            >
               <span className="font-medium">{`</>`}</span> Code
             </a>
           </div>
         </div>
-        <div className="flex flex-col items-center p-2">
+        <div className="flex flex-col items-center py-2 px-4">
           <img
-            className="mb-2"
-            src="https://placehold.co/300x200"
+            className="mb-2 drop-shadow-lg "
+            src={SkyRides}
             alt="picture of project"
           />
           <h2 className="text-xl font-bold mb-2">SkyRides</h2>
@@ -39,18 +52,26 @@ export default function Projects() {
             test@me.com, pw: 1010).
           </p>
           <div className="flex gap-2">
-            <a className="cursor-pointer bg-secondary text-primary font-semibold rounded-md py-0.5 px-4">
+            <a
+              className="cursor-pointer bg-secondary text-primary font-semibold rounded-md py-0.5 px-4"
+              href="https://skyrides.netlify.app"
+              target="_blank"
+            >
               <span className="font-medium">💿</span> Demo
             </a>
-            <a className="cursor-pointer bg-secondary text-primary font-semibold rounded-md py-0.5 px-4">
+            <a
+              className="cursor-pointer bg-secondary text-primary font-semibold rounded-md py-0.5 px-4"
+              href="https://github.com/priegelsdev/sky-rides"
+              target="_blank"
+            >
               <span className="font-medium">{`</>`}</span> Code
             </a>
           </div>
         </div>{' '}
-        <div className="flex flex-col items-center p-2">
+        <div className="flex flex-col items-center py-2 px-4">
           <img
-            className="mb-2"
-            src="https://placehold.co/300x200"
+            className="mb-2 drop-shadow-lg "
+            src={Wordle}
             alt="picture of project"
           />
           <h2 className="text-xl font-bold mb-2">Copy Wordle</h2>
@@ -59,18 +80,26 @@ export default function Projects() {
             two color and language options.
           </p>
           <div className="flex gap-2">
-            <a className="cursor-pointer bg-secondary text-primary font-semibold rounded-md py-0.5 px-4">
+            <a
+              className="cursor-pointer bg-secondary text-primary font-semibold rounded-md py-0.5 px-4"
+              href="https://copy-wordle.netlify.app"
+              target="_blank"
+            >
               <span className="font-medium">💿</span> Demo
             </a>
-            <a className="cursor-pointer bg-secondary text-primary font-semibold rounded-md py-0.5 px-4">
+            <a
+              className="cursor-pointer bg-secondary text-primary font-semibold rounded-md py-0.5 px-4"
+              href="https://github.com/priegelsdev/wordle"
+              target="_blank"
+            >
               <span className="font-medium">{`</>`}</span> Code
             </a>
           </div>
         </div>{' '}
-        <div className="flex flex-col items-center p-2">
+        <div className="flex flex-col items-center py-2 px-4">
           <img
-            className="mb-2"
-            src="https://placehold.co/300x200"
+            className="mb-2 drop-shadow-lg "
+            src={Portfolio}
             alt="picture of project"
           />
           <h2 className="text-xl font-bold mb-2">
