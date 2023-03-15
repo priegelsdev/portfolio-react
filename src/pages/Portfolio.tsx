b86381b1f3072bbc902bba13668897340f9ae1f6
priegelsdev <priegelsdev@gmail.com>
2023-03-15 12:20:33 +0100

    change content

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,11 +1,11 @@
 export default function Portfolio() {
   return (
     <div className="h-full flex flex-col bg-primary py-4 px-6">
-      <h1 className="text-3xl font-bold mb-2">Why another portfolio?</h1>
+      <h1 className="text-3xl font-bold ">A blog entry..? 🤨</h1>
       <h4 className="italic mb-2">March 14, 2023</h4>
-      <p className="text-justify">
-        A blog entry...? If you are not interested in reading about the reason
-        behind this page, feel free to head on over to the (old){' '}
+      <h3 className="text-sm font-medium mb-2">
+        (If you are not interested in reading about the reason behind this page,
+        feel free to head on over to the (old){' '}
         <a
           href="https://priegels.github.io/portfolio-website"
           target="_blank"
@@ -13,26 +13,31 @@ export default function Portfolio() {
         >
           portfolio
         </a>{' '}
-        directly. <br /> <br /> I started coding in 2021 with a bootcamp that
-        taught me a <span className="font-semibold">bunch</span> of technologies
-        in a relatively short amount of time after my full time day job. Fully
+        directly.)
+      </h3>
+
+      <p className="text-justify">
+        I started coding in 2021 with a bootcamp that taught me a{' '}
+        <span className="font-semibold">bunch</span> of technologies in a
+        relatively short amount of time after my full time day job. Fully
         motivated, I blazed through the program hoping to transition to tech
         asap. Little did I know though, learning to code takes time and practice
         and after diving into frontend, backend, different frameworks and
-        libraries, at the end of it all I realized: <i>I still cannot code</i>
+        libraries, at the end of it all, my head was spinning and I realized:{' '}
+        <i>Hey, what the.. I still cannot code</i>
         ...
         <br />
         The concepts remained foreign to me, the syntax was shaky at best and my
         ability to understand what I was doing was close to nil.
         <br />
-        <br />I finished the bootcamp, but I was not ready at all and was left
-        with only one choice: Quit my job and dedicate a few months to learning
-        coding for real. I had already peeked into how a lot of this was
-        supposed to work and I really enjoyed frontend work and working with
-        React. So I set out to teach myself this time around and I am happy to
-        say I feel great about my progress now as opposed to after the bootcamp.
-        I took away a lot from it still, so without further ado, here's me
-        showing off what I learned during it.
+        <br />I finished the bootcamp, but did not feel ready at all and was
+        left with only one choice: Dedicate a few months to learning coding for
+        real without any distractions. I had already peeked into how a lot of
+        this was supposed to work and I really enjoyed frontend work and working
+        with React. So I set out to <i>teach myself</i> this time around and I
+        am happy to say I feel great about my progress now as opposed to after
+        the bootcamp. I took away a lot from it still, so without further ado,
+        here's me showing off what I learned and produced during it.
       </p>
       <a
         href="https://priegels.github.io/portfolio-website"
